6cbc2980394d9da47ec543e6caf54c7f02d607e8
thimi0412 <thimi0412@icloud.com>
2018-07-29 00:45:45 +0900

    add
    ファイルを複数指定可能に

--- main.go
+++ main.go
@@ -8,21 +8,47 @@ import (
 )
 
 var (
-	intOpt = flag.Int("n", 10, "help message for \"n\" option")
+	n = flag.Int("n", 10, "show line")
 )
 
 func main() {
 	flag.Parse()
 	args := flag.Args()
 
-	lines := readFile(args[0])
-	lastIndex := len(lines) - 1
-
-	for i := lastIndex - *intOpt; i < lastIndex; i++ {
-		fmt.Println(lines[i])
+	if len(args) == 0 {
+		fmt.Println("ファイルを指定してください")
+		os.Exit(0)
 	}
 
+	// コマンドライン引数の数回す
+	for _, file := range args {
+		fmt.Printf("==> %s <==\n", file)
+		lines := readFile(file)
+
+		// 空ファイルは飛ばす
+		if lines == nil {
+			continue
+		}
+		
+		lastIndex := len(lines) - 1
+
+		var showStart int = lastIndex - *n
+		// ファイルの行数がデフォルト値(10)かn(任意の数)より小さければ
+		// showRangeを0とし表示できるだけ表示する
+		if showStart < 0 {
+			showStart = 0
+		}
+		showLines := lines[showStart:]
+		
+
+		for _, line := range showLines {
+			fmt.Println(line)
+		}
+		
+		fmt.Println("/////////////////////////////////////")
+	}
 }
+
 
 func readFile(file string) []string {
 	fp, err := os.Open(file)
@@ -30,7 +56,7 @@ func readFile(file string) []string {
 
 	if err != nil {
 		fmt.Println("ファイルが存在しません")
-		os.Exit(0)
+		return nil
 	}
 
 	lines := []string{}
